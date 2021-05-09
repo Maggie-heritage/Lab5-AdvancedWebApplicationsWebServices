b25121daeafe9e5992d7a54fb3dffd4d9dd44502
Maggie-heritage <coding0202@outlook.com>
2021-05-08 22:05:00 -0400

    submit lab

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const path = require('path');
+const dbLayer = require('./config/db');
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
+const app = express();
+const port = 9000;
+const { isRegExp } = require('util');
+const auth = require('./middleware/auth');
+
+const User = require('./model/User');
+const e = require('express');
+const { user } = require('./config/dbConn');
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(auth);
+
+app.get('/login', async (req, res) => {
+
+	res.json(req.auth);
+
+});
+
+app.all('/createuser', (req, res) => 
+{
+
+	try 
+	{
+		let newUser = req.body.newUser;
+		let newPass = req.body.newPass;
+
+		const hash = crypto.createHash("sha256").update(newPass).digest("hex");
+		User.addUser(newUser,hash);
+
+		res.json({ status: "Success" });
+	}
+	catch(err) 
+	{
+
+	}
+
+
+
+
+
+
+
+/* 	if (req.auth.isAuth) {
+		let newUser = req.body.newUser;
+		let newPass = req.body.newPass;
+
+		const hash = crypto.createHash("sha256").update(newPass).digest("hex");
+		User.addUser(newUser,hash);
+
+		res.json({ status: "Success" });
+	} else {
+		res.json({ status: "Failed" });
+	} */
+});
+
+
+
+app.listen(port, () => {
+	dbLayer.init();
+	console.log(`listening on port: ${port}`);
+});
---
+++ model/User.js
@@ -0,0 +1,29 @@
+const db = require('../config/db');
+
+module.exports = class {
+	static async getUser(user, hash) {
+		let connection = await db.getConnection();
+		const rows = await connection.query(
+			'SELECT userId, user FROM `user` WHERE `user` = ? AND `hash` = ? LIMIT 1',
+			[user, hash]
+		);
+		if (rows.length > 0) {
+			return { user: rows[0] };
+		}
+
+		return { user: null };
+	}
+
+	static async addUser(user, hash) 
+	{
+		let connection = await db.getConnection();
+		const rows = await connection.query(
+			"INSERT INTO `user`(`User`,`Hash`) VALUES(?, ? ) ", [user, hash]
+		);
+		if (rows.length > 0) {
+			return { user: rows[0] };
+		}
+
+		return { user: null };
+	}
+};
